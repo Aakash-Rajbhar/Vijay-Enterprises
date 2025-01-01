5f1a30b35a1acf9fddcf4a56116b1bbb65136518
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2025-01-01 17:26:48 +0530

    solved dark and light color on homescreen

--- src/components/ui/TextGenerateEffect.jsx
+++ src/components/ui/TextGenerateEffect.jsx
@@ -33,7 +33,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0 text-3xl tracking-widest md:text-4xl lg:text-6xl"
+              className="text-white opacity-0 text-3xl tracking-widest md:text-4xl lg:text-6xl"
               style={{
                 filter: filter ? 'blur(10px)' : 'none',
               }}
@@ -49,7 +49,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn('font-bold', className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className="text-white text-2xl leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
